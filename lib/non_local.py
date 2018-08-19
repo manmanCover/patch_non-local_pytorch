8cf8b0dbd854903eabc7eab53f9095a5ac29b63e
Dongxian Wu <wu-dx16@mails.tsinghua.edu.cn>
2018-08-19 22:42:16 +0800

    delete useless func
    _concatenation & _dot_product

--- lib/non_local.py
+++ lib/non_local.py
@@ -6,14 +6,6 @@ from torch.nn import functional as F
 class _NonLocalBlockND(nn.Module):
     def __init__(self, in_channels, inter_channels=None, dimension=2, mode='patch_embedded_gaussian',
                  sub_sample=True, bn_layer=True, nb_patches=None):
-        """
-        :param in_channels:
-        :param inter_channels:
-        :param dimension:
-        :param mode:
-        :param sub_sample:
-        :param bn_layer:
-        """
         super(_NonLocalBlockND, self).__init__()
 
         assert dimension in [2]
@@ -186,59 +178,6 @@ class _NonLocalBlockND(nn.Module):
 
         f = torch.matmul(theta_x, phi_x)
         f_div_C = F.softmax(f, dim=-1)
-
-        y = torch.matmul(f_div_C, g_x)
-        y = y.permute(0, 2, 1).contiguous()
-        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
-        W_y = self.W(y)
-        z = W_y + x
-
-        return z
-
-    def _dot_product(self, x):
-        batch_size = x.size(0)
-
-        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
-        g_x = g_x.permute(0, 2, 1)
-
-        theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
-        theta_x = theta_x.permute(0, 2, 1)
-        phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
-        f = torch.matmul(theta_x, phi_x)
-        N = f.size(-1)
-        f_div_C = f / N
-
-        y = torch.matmul(f_div_C, g_x)
-        y = y.permute(0, 2, 1).contiguous()
-        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
-        W_y = self.W(y)
-        z = W_y + x
-
-        return z
-
-    def _concatenation(self, x):
-        batch_size = x.size(0)
-
-        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
-        g_x = g_x.permute(0, 2, 1)
-
-        # (b, c, N, 1)
-        theta_x = self.theta(x).view(batch_size, self.inter_channels, -1, 1)
-        # (b, c, 1, N)
-        phi_x = self.phi(x).view(batch_size, self.inter_channels, 1, -1)
-
-        h = theta_x.size(2)
-        w = phi_x.size(3)
-        theta_x = theta_x.repeat(1, 1, 1, w)
-        phi_x = phi_x.repeat(1, 1, h, 1)
-
-        concat_feature = torch.cat([theta_x, phi_x], dim=1)
-        f = self.concat_project(concat_feature)
-        b, _, h, w = f.size()
-        f = f.view(b, h, w)
-
-        N = f.size(-1)
-        f_div_C = f / N
 
         y = torch.matmul(f_div_C, g_x)
         y = y.permute(0, 2, 1).contiguous()
